b211a1f0a9f0a3094c9b900fab9ebcbaf0a68094
princekalra <kalraprince75@gmail.com>
2024-04-27 13:40:24 +0530

    added changes for introduction

---
+++ introduction.js
@@ -0,0 +1,54 @@
+// Write a single line comment which says, comments can make code readable
+
+// comments can make code readable
+
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+// Welcome to 30DaysOfJavaScript
+
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+/**comments can make code readable, 
+ * easy to reuse and informative */
+
+
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let var1 = "string";
+var1 = true;
+var1 = undefined;
+var1 = null
+
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof "string");
+console.log(typeof 12);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+// Declare four variables without assigning values
+let first_var,second_var,third_var,fourth_var;
+console.log(first_var, second_var, third_var, fourth_var);
+
+// Declare four variables with assigned values
+let a = 1,b=2,c=3,d=5;
+console.log(a,b,c,d);
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = "akshay";
+let lastName = "saini";
+let country = "India";
+let maritalStatus = "Married";
+let age = 30;
+console.log(firstName,lastName,country,maritalStatus,age);
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+let firstName1 = "akshay",lastName1 = "saini",country1 = "India",maritalStatus1 = "Married",age1 = 30;
+console.log(firstName1, lastName1, country1, maritalStatus1, age1);
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console
+let myAge = 1;
+let yourAge = 12;
+console.log(myAge,yourAge);
